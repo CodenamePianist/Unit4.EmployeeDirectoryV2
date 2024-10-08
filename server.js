48879916b75b41af6b9068ab644b5962f1c598b4
Brandon Hamilton <bham2015@gmail.com>
2024-10-08 13:30:23 -0500

    Fixed logic so the nonexistent employee scenario works.

--- server.js
+++ server.js
@@ -6,26 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-const employees = require("./employees");
-
-app.get("/employees", (req, res) => {
-  res.json(employees);
-});
-
-app.get("/employees/random", (req, res) => {
-  const i = Math.floor(Math.random() * employees.length);
-  res.json(employees[i]);
-});
-
-app.get("/employees/:id", (req, res, next) => {
-  const { id } = req.params;
-  const employee = employees.find((e) => e.id === +id);
-  if (employee) {
-    res.json(employee);
-  } else {
-    next({ status: 404, message: `There is no employee with id ${id}.` });
-  }
-});
+app.use("/employees", require("./api/employees"));
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Sorry, this doesn't exist." });
